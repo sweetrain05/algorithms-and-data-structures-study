b00ed6670ea7f5fcf9ebca4f760c5b1437ff61aa
Danbi Choi <sweetrain05@hotmail.com>
2023-12-09 17:41:25 -0800

    Dec. 9th, 2023 Leetcode 75

---
+++ leetcode_problem solving/2215. Find the Difference of Two Arrays.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/find-the-difference-of-two-arrays/description/?envType=study-plan-v2&envId=leetcode-75
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function (nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+
+    for (let num of set1) {
+        if (set2.has(num)) {
+            set1.delete(num);
+            set2.delete(num);
+        }
+    }
+    return [[...set1], [...set2]];
+};
