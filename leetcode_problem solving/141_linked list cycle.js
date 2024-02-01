2adc9fc91eef5e577d058b4cc36d670de7f63139
Danbi Choi <sweetrain05@hotmail.com>
2024-01-31 23:04:06 -0800

    .

--- leetcode_problem solving/141_linked list cycle.js
+++ leetcode_problem solving/141_linked list cycle.js
@@ -1,3 +1,8 @@
+//https://leetcode.com/problems/linked-list-cycle/description/
+
+//Solution 1
+// using Array and includes method
+// TC: O(N^2), SC: O(N)
 var hasCycle = function (head) {
     let arr = [];
     let curr = head;
@@ -10,5 +15,39 @@ var hasCycle = function (head) {
             return true;
         }
     }
+    return false;
+};
+
+//Solution 2
+// using JS Set
+// TC: O(N), SC: O(N)
+var hasCycle2 = function (head) {
+    let visited = new Set();
+    let curr = head;
+
+    while (curr && curr.next) {
+        if (!visited.has(curr)) {
+            visited.add(curr);
+            curr = curr.next;
+        } else {
+            return true;
+        }
+    }
+    return false;
+};
+
+//Solution 3
+// using two pointer
+// TC: O(N), SC: O(1)
+var hasCycle3 = function (head) {
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast === slow) return true;
+    }
+
     return false;
 };
